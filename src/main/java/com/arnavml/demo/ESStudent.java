d39a021ca256806448d9c51d6890ca40da621b74
Arnav Swami <arnavswami03@gmail.com>
2021-05-17 23:07:39 -0700

    First stuff

---
+++ src/main/java/com/arnavml/demo/ESStudent.java
@@ -0,0 +1,14 @@
+package com.arnavml.demo;
+
+public class ESStudent extends Student {
+
+    int gradeLevel;
+
+    public ESStudent(String n, int l) {
+        super(n);
+        gradeLevel = l;
+    }
+
+
+
+}
---
+++ src/main/java/com/arnavml/demo/MSStudent.java
@@ -0,0 +1,18 @@
+package com.arnavml.demo;
+public class MSStudent extends Student {
+
+    int gradeLevel;
+
+    public MSStudent(String n, int l) {
+        super(n);
+        gradeLevel = l;
+    }
+    @Override
+    public Boolean registerforclass(String className) {
+        System.out.println("Registering " + name + " for " + className + " in grade " + gradeLevel);
+        if (getParentApproval()) {
+            return Boolean.TRUE;
+        }
+        return Boolean.FALSE;
+    }
+}
